88a157f4229ecfe2ef07fb2da2e2afb8607553f1
David Mayer <david.mayer@cuanschutz.edu>
2025-12-01 15:57:49 -0500

    prep for google analytics inclusion

--- R/export.R
+++ R/export.R
@@ -57,6 +57,48 @@ export_excel <- function(core_project_numbers, token = gitcreds::gitcreds_get()$
   github_info <- get_github_by_topic_graphql(core_project_numbers, token = token)
   writeData(wb = wb, sheet = "github_info", x = github_info, na.string = "")
 
+  ## Add Google Analytics
+  addWorksheet(wb, "ga_info")
+  json_file <- gargle::secret_decrypt_json(
+    path = system.file(
+      "secret",
+      "cfde-access-keyfile.json",
+      package = "programets"
+    ),
+    key = "CFDE_ENCRYPTION_KEY"
+  )
+  googleAnalyticsR::ga_auth(
+    json_file = json_file
+  )
+  account_list <- ga_account_list("ga4") |> 
+    mutate(
+      property_meta = map(propertyId, get_ga_meta_by_id),
+      core_project_num = map_chr(
+        property_meta,
+        ~{
+          res <- .x |>
+            filter(str_detect(apiName, core_project_numbers)) |>
+            tidyr::separate(apiName, into = c("api", "value"), sep = ":", remove = FALSE) |>
+            pull(value)
+
+          if (length(res) == 0) {
+            NA_character_
+          } else {
+            res |>
+              str_remove("^cfde_") |>
+              unique() |>
+              paste(collapse = ",")
+          }
+        }
+      )
+    ) |> 
+    filter(!is.na(core_project_num)) |> 
+    select(-property_meta)
+  writeData(wb = wb, sheet = "ga_info", x = account_list, na.string = "")
+
+  ga_info <- get_ga_meta_by_id(core_project_numbers)
+  writeData(wb = wb, sheet = "ga_info", x = ga_info, na.string = "")
+
   ## Save Workbook
   saveWorkbook(wb, paste0("programets_", Sys.Date(), ".xlsx", sep = ""))
 }
