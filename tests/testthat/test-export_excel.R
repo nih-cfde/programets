010157c02fabe03d526a27d97dcbd75c366571fc
David Mayer <david.mayer@cuanschutz.edu>
2025-12-04 08:35:22 -0500

    add test for export excel

---
+++ tests/testthat/test-export_excel.R
@@ -0,0 +1,20 @@
+library(gitcreds)
+
+test_that("export_excel writes an Excel file", {
+  temp_file <- tempfile(fileext = ".xlsx")
+  on.exit(unlink(temp_file))
+  core_project_numbers <- c("OT2OD030545")
+  token <- gitcreds_get()$password
+  export_excel(core_project_numbers = core_project_numbers, token = token, file = temp_file)
+  expect_true(file.exists(temp_file))
+})
+
+test_that("export_excel throws an error if the file already exists", {
+  temp_file <- tempfile(fileext = ".xlsx")
+  on.exit(unlink(temp_file))
+  core_project_numbers <- c("OT2OD030545")
+  token <- gitcreds_get()$password
+  file.create(temp_file)
+  expect_error(export_excel(core_project_numbers = core_project_numbers, token = token, file = temp_file), "File already exists")
+})
+
